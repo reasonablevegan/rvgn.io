f94ec23d654b9e563edf5acc4f38019a2cc89edd
Paul Fox <paul@homemadedigital.com>
2014-08-10 11:06:07 +0100

    New header

--- source/javascripts/rvgn.js
+++ source/javascripts/rvgn.js
@@ -5,4 +5,28 @@ $(document).on("ready",function(){
 	    return $(this).attr('src').replace('.svg', '.png');
 	  });
 	}
+
+  // Blurring concerns
+  var BLUR_RADIUS = 80;
+
+  var canvas = document.getElementById("heroCanvas");
+
+  if (canvas) {
+    var canvasContext = canvas.getContext('2d');
+    
+    var image = new Image();
+    image.src = $('[data-blurimage]')[0].src;
+    
+    var drawBlur = function() {
+      var w = canvas.width;
+      var h = canvas.height;
+      canvasContext.drawImage(image, 0, 0, w, h);
+      stackBlurCanvasRGBA('heroCanvas', 0, 0, w, h, BLUR_RADIUS);
+      $(canvas).addClass('fade-in');
+    };
+    
+    image.onload = function() {
+      drawBlur();
+    }
+  }
 });
